5ecbf8f9a769704504a67eb58851204f3c288c6a
LUO Boming <luo.boming@fixstars.com>
2019-07-01 18:37:59 +0900

    add prototype app

---
+++ app.rb
@@ -0,0 +1,76 @@
+require 'pathname'
+require 'optparse'
+require 'time'
+
+require 'twitter'
+require 'redis'
+
+
+def fetch_timeline_tweets
+    p "fetch from twitter"
+
+    twitter_client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+        config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+        config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    end
+
+    return twitter_client.home_timeline(count: 3200, include_rts: true).map(&:attrs)
+end
+
+def shrink_tweet(tweet)
+    shrinked = {}
+
+    shrinked[:id] = tweet['id']
+    shrinked[:timestamp] = Time.parse(tweet['created_at']).iso8601
+    shrinked[:screen_name] = tweet['user']['screen_name']
+    shrinked[:type] = -> {
+        return 'retweet' if tweet['retweeted_status']
+        return 'reply'   if tweet['in_reply_to_status_id']
+        return 'quoted'  if tweet['quoted_status']
+        return 'normal'
+    }.call
+
+    shrinked
+end
+
+def save_to_redis(tweets, redis)
+    # TODO: get timestamp of latest stored tweet in redis
+
+    tweets = tweets.reverse # storing from older one
+    tweets.each do |attr|
+        attr.each_pair do |key,val|
+            redis.lpush("tweets:#{key}", val)
+        end
+    end
+end
+
+if $0 == __FILE__
+    opt_parser = OptionParser.new
+    opt_parser.on('-t', '--test')
+    opt_parser.on('-s JSON', '--serialize=JSON') { |v| Pathname.new(v) }
+
+    options = {}
+    opt_parser.parse!(into: options)
+
+    if options.include?(:test)
+        require 'dotenv'
+        Dotenv.load
+    end
+
+    json_path = options.fetch(:serialize, nil)
+    tweets = JSON.load(json_path) if json_path&.file?
+    tweets ||= fetch_timeline_tweets
+    p tweets.size
+    JSON.dump(tweets, json_path)
+
+    begin
+        shrinked = tweets.map { |t| shrink_tweet(t) }
+        redis = Redis.new(url: ENV['REDIS_URL'])
+        #save_to_redis(shrinked, redis)
+    rescue => err
+        p err
+        raise
+    end
+end
