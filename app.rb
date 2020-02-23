9b9419807db4e00d961a895960b51c42cba44285
Luo Boming <la_float_765millionstars@yahoo.co.jp>
2020-02-24 00:38:03 +0900

    add functino to post song links to slack

--- app.rb
+++ app.rb
@@ -1,98 +1,80 @@
-require 'pathname'
-require 'optparse'
-require 'logger'
+require "pathname"
+require "optparse"
+require "logger"
 
-require 'twitter'
-require 'redis'
+require "twitter"
+require "redis"
 
-require_relative 'lib/frequency'
-
-
-def main
-    # Parse option
-    options = {}
-    opt_parser = OptionParser.new
-    opt_parser.on('-t', '--test')
-    opt_parser.on('-s JSON', '--serialize=JSON') { |v| Pathname.new(v) }
-    opt_parser.parse!(into: options)
-
-    # Logger
-    logger = Logger.new($stderr, progname: 'TweetCurator')
-
-    if options[:test]
-        require 'dotenv'
-        Dotenv.load
-    end
-
-    json_path = options[:serialize]
-    # NOTE: JSON.load in Ruby 2.6.3 has a bug, using parse instead
-    tweets = JSON.parse(json_path.read, symbolize_names: true) if json_path&.file?
-    tweets ||= fetch_timeline_tweets
-    JSON.dump(tweets, json_path)
-
-    call_without_abort(logger: logger) do
-        frequency_pipeline(tweets, logger: logger)
-    end
-end
-
-#------------------------------------------------------------------------------
-# Pipeline
-#------------------------------------------------------------------------------
+require_relative "lib/frequency"
+require_relative "lib/songlink"
 
 # Fetch twitter home timeline
-def fetch_timeline_tweets
-    twitter_client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-        config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-        config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-        config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-    end
+def fetch_timeline_tweets(twitter_client, since_id = nil)
+  # 'home_timeline' API can retrieve upto 200 tweets
+  # Since 800 tweets are available, we call it 4 times
+  max_id = nil
+  total_tweets = []
+  4.times do |i|
+    opts = { count: 200, include_rts: true }
+    opts[:max_id] = max_id if max_id
+    opts[:since_id] = since_id if since_id
+    tweets = twitter_client.home_timeline(opts)
+    break if tweets.empty?
 
-    # 'home_timeline' API can retrieve upto 200 tweets
-    # Since 800 tweets are available, we call it 4 times
-    total_tweets = []
-    max_id = nil
-    4.times do |i|
-        opts = {count: 200, include_rts: true}
-        opts[:max_id] = max_id if max_id
-        tweets = twitter_client.home_timeline(opts).map(&:attrs)
+    last_id = tweets.last.attrs[:id]
+    tweets.shift if max_id
+    max_id = last_id
 
-        last_id = tweets.last[:id]
-        tweets.shift if max_id
-        max_id = last_id
+    total_tweets += tweets
+  end
 
-        total_tweets += tweets
-    end
-
-    total_tweets
+  total_tweets
 end
 
 def call_without_abort(logger: nil, &block)
-    begin
-        yield
-    rescue => err
-        if logger
-            logger.error("Error: " + err.full_message)
-        else
-            puts err.full_message
-        end
+  begin
+    yield
+  rescue => err
+    if logger
+      logger.error("Error: " + err.full_message)
+    else
+      puts err.full_message
     end
+  end
 end
 
-def frequency_pipeline(tweets, logger: nil)
-    min_tweets = tweets.map { |t| Frequency::MinTweet.from_tweet(t) }
-    redis = Frequency::MinTweetRedis.new(Redis.new(url: ENV['REDIS_URL']))
+def main
+  options = {}
+  opt_parser = OptionParser.new
+  opt_parser.on("-t", "--test", "use local dotenv config")
+  opt_parser.parse!(into: options)
 
-    recent_ids = redis.restore(0...5, use_keys: [:id]).map(&:id)
-    stop_idx = min_tweets.find_index { |t| recent_ids.include?(t.id) }
-    min_tweets = min_tweets[0...stop_idx]
+  if options[:test]
+    require "dotenv"
+    Dotenv.load
+  end
 
-    min_tweets.reverse.each { |t| redis.store(t) }
-    logger&.info { "Stored #{min_tweets.size} tweets in Redis" }
+  logger = Logger.new($stderr, progname: "TweetCurator")
+
+  twitter_client = Twitter::REST::Client.new do |config|
+    config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+
+  # fetch timeline using API
+  redis = Redis.new(url: ENV["REDIS_URL"]) if ENV.include?("REDIS_URL")
+  since_id = redis&.get("since_id")
+  tweets = fetch_timeline_tweets(twitter_client, since_id)
+  redis&.set("since_id", tweets.first&.attrs[:id])
+
+  # pipelines
+  if ENV.include?("SLACK_WEBHOOK_URL")
+    call_without_abort(logger: logger) do share_media_link_to_slack(tweets, ENV["SLACK_WEBHOOK_URL"]) end
+  end
 end
-
 
 if $0 == __FILE__
-    main
+  main
 end
-
---
+++ lib/songlink.rb
@@ -0,0 +1,37 @@
+require "net/http"
+require "uri"
+
+SONG_URL_DOMAINS = %w[
+  song.link
+  youtube.com
+  youtu.be
+  music.apple.com
+  open.spotify.com
+]
+
+def share_media_link_to_slack(tweets, slack_webhook_url, logger: nil)
+  # slack API
+  uri = URI.parse(slack_webhook_url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  post_to_slack = Proc.new do |text|
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req["Content-Type"] = "application/json"
+    req.body = {
+      text: text,
+    }.to_json
+  end
+
+  tweets.each do |t|
+    urls = t.attrs[:entities][:urls]&.filter { |url| SONG_URL_DOMAINS.any? { |dom| url[:expanded_url].include?(dom) } }
+    unless urls.empty?
+      # tweet URL
+      post_to_slack.call(t.url)
+
+      # song URL
+      urls.each { |url| post_to_slack.call(url[:expanded_url]) }
+    end
+  end
+end
