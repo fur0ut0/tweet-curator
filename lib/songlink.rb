3e476ea097ad67bf806ba6917a1f2fad627e162b
Luo Boming <la_float_765millionstars@yahoo.co.jp>
2020-02-24 00:53:20 +0900

    use manual attachment for formatting

--- lib/songlink.rb
+++ lib/songlink.rb
@@ -17,12 +17,10 @@ def share_media_link_to_slack(tweets, slack_webhook_url, logger: nil)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-  post_to_slack = Proc.new do |text|
+  post_to_slack = Proc.new do |hash|
     req = Net::HTTP::Post.new(uri.request_uri)
     req["Content-Type"] = "application/json"
-    req.body = {
-      text: text,
-    }.to_json
+    req.body = hash.to_json
     http.request(req)
   end
 
@@ -33,12 +31,27 @@ def share_media_link_to_slack(tweets, slack_webhook_url, logger: nil)
       SONG_URL_DOMAINS.any? { |dom| url[:expanded_url].include?(dom) }
     end
 
-    unless urls.empty?
-      # tweet URL
-      post_to_slack.call(t.url)
+    next if urls.empty?
 
-      # song URL
-      urls.each { |url| post_to_slack.call(url[:expanded_url]) }
+    # tweet
+    post_to_slack.call({
+      attachments: [
+        {
+          title: "#{t.attrs[:user][:name]} (@#{t.attrs[:user][:screen_name]})",
+          title_link: t.url,
+          text: t.attrs[:text],
+          thumb_url: t.attrs[:user][:profile_image_url_https],
+          footer: t.attrs[:created_at],
+        },
+      ],
+    })
+
+    # song link
+    # TODO: expand apple music
+    urls.each do |url|
+      post_to_slack.call({
+        text: url[:expanded_url],
+      })
     end
   end
 end
