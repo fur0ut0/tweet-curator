e1debf49c09ab8ec11aa0664b53f8f36dc21c6f8
Luo Boming <la_float_765millionstars@yahoo.co.jp>
2021-04-25 11:37:36 +0900

    Add a new pattern for music service
    * orcd.co (feature.fm)

--- tweet_curator/media_task.rb
+++ tweet_curator/media_task.rb
@@ -11,7 +11,7 @@ module TweetCurator
 
       IMAGE_HOST = %w[pbs.twimg.com].freeze
 
-      GENERAL_MUSIC_HOST = %w[linkco.re big-up.style nowplaying.jp].freeze
+      GENERAL_MUSIC_HOST = %w[linkco.re big-up.style nowplaying.jp orcd.co].freeze
       GENERAL_MUSIC_HOST_RE = [/.*soundcloud*/].freeze
 
       CONVERTIBLE_MUSIC_HOST = %w[song.link album.link open.spotify.com music.amazon.co.jp].freeze
